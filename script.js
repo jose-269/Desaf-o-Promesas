5c90f4204f1474ad7c5efb8ba83e7632f0da5238
unknown <jlmv84Qgmail.com>
2021-03-26 12:22:34 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const consumirApi = async () => {
+    const url = "https://jsonplaceholder.typicode.com/photos";
+
+    try{
+        const dataRaw = await fetch(url);
+        const data = await dataRaw.json();
+        const dataArr = data;
+
+        const titles = await dataArr.forEach((el, index) => {
+        if(index < 20) {
+            return  console.log(el.id, el.title);
+        };
+        })
+            
+    }catch(error) {
+        console.log(error);
+    };
+};
+
+
+const info = async () =>{
+    const msje = () =>  console.log("Información Enviada");
+   return setTimeout((msje),3000);
+} 
+
+
+(async() => {
+    await consumirApi();
+    const mensaje = await (info());
+    
+})();
+
+
+
+
